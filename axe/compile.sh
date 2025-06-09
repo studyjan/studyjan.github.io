1ba17ae54806bd8225aa2e7b6e7abf71b94158f5
Jan Vonka <jan.vonka@ataccama.com>
2025-06-09 12:14:37 +0200

    zips

--- axe/compile.sh
+++ axe/compile.sh
@@ -1,2 +1,31 @@
 #!/bin/bash
 
+for filename in ./banner*.html; do
+    echo "$filename:"
+    basename="${filename%.html}"
+    mkdir "$basename"
+    cp $filename $basename/index.html
+    cp "style.css" $basename
+    cp -r build $basename
+    cp -r fonts $basename
+    for img in ./*.png; do
+        cp $img $basename
+    done
+
+    size_part="${basename#*_}"
+    size_part="${size_part#*_}"
+    size="${size_part%%_*}"
+
+    width="${size%x*}"
+    height="${size#*x}"
+
+    echo "FT.manifest({
+    \"filename\": \"index.html\",
+    \"width\": $width,
+    \"height\": $height,
+    \"clickTagCount\": 1
+});" > $basename/manifest.js
+
+    zip -r $basename.zip $basename > /dev/null
+    rm -rf $basename
+done
